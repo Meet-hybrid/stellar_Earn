b3964155752429e1f84869c447a93b0bf608704b
Halimat <nimatyisau@gmail.com>
2026-06-18 20:29:34 +0100

    style: apply prettier formatting to auth.e2e-spec.ts

--- BackEnd/test/auth.e2e-spec.ts
+++ BackEnd/test/auth.e2e-spec.ts
@@ -1,4 +1,4 @@
-﻿import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
